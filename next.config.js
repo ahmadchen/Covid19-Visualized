3be78464389c38e587f65f8904a51d60cdbf1f99
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2020-04-07 03:19:27 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withSass = require('@zeit/next-sass')
+const withPWA = require('next-offline')
+const TsconfigPathsWebpackPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = withPWA(withSass({
+    dir: './src',
+    webpack: config => {
+        if (config.resolve.plugins) {
+            config.resolve.plugins.push(new TsconfigPathsWebpackPlugin())
+        } else {
+            config.resolve.plugins = [new TsconfigPathsWebpackPlugin()]
+        }
+
+        return config
+    }
+}))
