16b52acb32f2eb86cbd0c72f14048657c7d0b103
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2020-04-07 03:24:07 +0700

    Create metaGenerator.tsx

---
+++ src/utils/metaGenerator.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+
+interface Meta {
+    hid?: string
+    content?: string
+    name?: string
+}
+
+interface MetaProperty {
+    title: string
+    description: string
+    keywords: string
+}
+
+export default (meta: MetaProperty): ReactNode => {
+    const metaProperties: Meta[] = [
+        { hid: 'title', name: 'title', content: `${meta.title} (By: Pace Usa.)` },
+        { hid: 'description', name: 'description', content: `${meta.description} (Made by: Pace Usa.)` },
+        { hid: 'keywords', name: 'keywords', content: meta.keywords },
+        { hid: 'theme-color', name: 'theme-color', content: '#847cf3' },
+        { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#847cf3' },
+        { hid: 'msapplication-TileImage', name: 'msapplication-TileImage', content: '/favicon/ms-icon-144x144.png' }   
+    ]
+    
+    return metaProperties.map((props, idx) => <meta key={idx} {...props} />)
+}
